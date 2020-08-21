a8d91a83e3645611b0919912056d856f5af22300
qzk11 <616341668@qq.com>
2020-08-20 23:50:06 -0400

    Implement intefaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/com/zhekun/petclinic/services/PetService.java
@@ -0,0 +1,12 @@
+package com.zhekun.petclinic.services;
+
+
+import com.zhekun.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
