4b530a548e1950dbd6416430635b8bf0787f83e7
qzk11 <616341668@qq.com>
2020-08-25 00:54:00 -0400

    adding bootstrap for data closes #14

--- pet-clinic-data/src/main/java/com/zhekun/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/zhekun/petclinic/services/map/OwnerServiceMap.java
@@ -1,11 +1,11 @@
 package com.zhekun.petclinic.services.map;
 
 import com.zhekun.petclinic.model.Owner;
-import com.zhekun.petclinic.services.CrudService;
+import com.zhekun.petclinic.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -29,5 +29,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public void deleteById(Long id) {
         super.deleteById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
     }
 }
--- pet-clinic-data/src/main/java/com/zhekun/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/zhekun/petclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package com.zhekun.petclinic.services.map;
 
 import com.zhekun.petclinic.model.Vet;
-import com.zhekun.petclinic.services.CrudService;
+import com.zhekun.petclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     @Override
     public Set<Vet> findAll() {
