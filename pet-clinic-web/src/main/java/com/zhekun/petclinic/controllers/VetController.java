5a19e9c65e768022c27f1b6facbca732167803dc
qzk11 <616341668@qq.com>
2020-08-26 15:27:18 -0400

    adding Vet list and update controller. closes #15

--- pet-clinic-web/src/main/java/com/zhekun/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/zhekun/petclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package com.zhekun.petclinic.controllers;
 
+import com.zhekun.petclinic.services.VetService;
+import org.dom4j.rule.Mode;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
