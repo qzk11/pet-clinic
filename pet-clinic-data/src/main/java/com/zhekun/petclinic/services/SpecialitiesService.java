bfa524cd660b8f7c6ab60a626ff7625bea96174d
qzk11 <616341668@qq.com>
2020-08-31 19:13:03 -0400

    added specialities map impl, closes #31

---
+++ pet-clinic-data/src/main/java/com/zhekun/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,9 @@
+package com.zhekun.petclinic.services;
+
+import com.zhekun.petclinic.model.Speciality;
+
+import java.util.Set;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+}
