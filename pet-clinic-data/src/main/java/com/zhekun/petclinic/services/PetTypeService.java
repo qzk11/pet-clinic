a8e003d6cba860086ac81eb244ba9e5f379d3de7
qzk11 <616341668@qq.com>
2020-08-31 14:34:51 -0400

    added petTypeService, closes #29

---
+++ pet-clinic-data/src/main/java/com/zhekun/petclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.zhekun.petclinic.services;
+
+import com.zhekun.petclinic.model.PetType;
+import com.zhekun.petclinic.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
