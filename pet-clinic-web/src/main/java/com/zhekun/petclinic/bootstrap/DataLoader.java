f03a4a362aaa23073ef6a526bbe7491d53eb5b8b
qzk11 <616341668@qq.com>
2020-09-01 15:54:41 -0400

    Implemented JPA Visit Service, added loader data. closes #47

--- pet-clinic-web/src/main/java/com/zhekun/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/zhekun/petclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.zhekun.petclinic.bootstrap;
 
 import com.zhekun.petclinic.model.*;
-import com.zhekun.petclinic.services.OwnerService;
-import com.zhekun.petclinic.services.PetTypeService;
-import com.zhekun.petclinic.services.SpecialtyService;
-import com.zhekun.petclinic.services.VetService;
+import com.zhekun.petclinic.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -18,13 +15,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
 
@@ -93,6 +92,13 @@ public class DataLoader implements CommandLineRunner {
 
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
 
         System.out.println("Loaded Owners....");
 
