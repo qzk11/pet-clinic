bbf750586ee776c4b57facde25822fc9bdd73407
qzk11 <616341668@qq.com>
2020-09-01 17:39:34 -0400

    Added Project Lombok to project, closes #51

--- pet-clinic-data/src/main/java/com/zhekun/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/zhekun/petclinic/model/Visit.java
@@ -1,10 +1,16 @@
 package com.zhekun.petclinic.model;
 
+import lombok.*;
 import lombok.extern.apachecommons.CommonsLog;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "visits")
 public class Visit extends BaseEntity{
@@ -17,27 +23,5 @@ public class Visit extends BaseEntity{
     @JoinColumn(name = "pet_id")
     private Pet pet;
 
-    public LocalDate getDate() {
-        return date;
-    }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Pet getPet() {
-        return pet;
-    }
-
-    public void setPet(Pet pet) {
-        this.pet = pet;
-    }
 }
